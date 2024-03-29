755216fd9e53c8db56934250c58c49060dcf4ab6
Waleed-IEE <iee.twaleed@gmail.com>
2024-03-29 16:15:23 +0100

    fix: Add player input type checking and make it case insensitive

--- js/script.js
+++ js/script.js
@@ -4,12 +4,19 @@ function getComputerChoice(){
     :   random === 1 ? "paper"
     : "scissors";
 }
-function gameRound(playerChoice, computerChoice = getComputerChoice()){
-    return (playerChoice === "rock" && computerChoice === "scissors") 
-    || (playerChoice === "paper" && computerChoice === "rock")
-    || (playerChoice === "scissors" && computerChoice === "paper") ? alert(`You WIN!\nYour ${playerChoice} beats the opponent's ${computerChoice}.`)
-    : (computerChoice === "rock" && playerChoice === "scissors") 
-    || (computerChoice === "paper" && playerChoice === "rock")
-    || (computerChoice === "scissors" && playerChoice === "paper") ? alert(`You LOSE!\nThe opponent's ${computerChoice} beats your ${playerChoice}.`)
-    : "It's a TIE!";
+function gameRound(caseSensitivePlayerChoice, computerChoice = getComputerChoice()){
+    if (typeof caseSensitivePlayerChoice === "string"){
+        playerChoice = caseSensitivePlayerChoice.toLowerCase();
+        return (playerChoice === "rock" && computerChoice === "scissors") 
+            || (playerChoice === "paper" && computerChoice === "rock")
+            || (playerChoice === "scissors" && computerChoice === "paper") ? alert(`You WIN!\nYour ${playerChoice} beats the opponent's ${computerChoice}.`)
+            :  (computerChoice === "rock" && playerChoice === "scissors") 
+            || (computerChoice === "paper" && playerChoice === "rock")
+            || (computerChoice === "scissors" && playerChoice === "paper") ? alert(`You LOSE!\nThe opponent's ${computerChoice} beats your ${playerChoice}.`)
+            :  "It's a TIE!";
+    }
+    else{
+        alert("Invalid input! Please type either rock, paper or scissors to play the game...");
+    }
+
 }
