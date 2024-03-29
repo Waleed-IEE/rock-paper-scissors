0d174130dd335457c14885a9862c2c1efc25027f
Waleed-IEE <iee.twaleed@gmail.com>
2024-03-29 16:16:48 +0100

    fix: Update tie message to return alert popup message instead of a string

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function gameRound(caseSensitivePlayerChoice, computerChoice = getComputerChoice
             :  (computerChoice === "rock" && playerChoice === "scissors") 
             || (computerChoice === "paper" && playerChoice === "rock")
             || (computerChoice === "scissors" && playerChoice === "paper") ? alert(`You LOSE!\nThe opponent's ${computerChoice} beats your ${playerChoice}.`)
-            :  "It's a TIE!";
+            :  alert("It's a TIE!");
     }
     else{
         alert("Invalid input! Please type either rock, paper or scissors to play the game...");
