5900d8f789f98a6a2053c83e2252b405646f7322
Waleed-IEE <iee.twaleed@gmail.com>
2024-03-29 15:59:50 +0100

    feat: Add a function for a single game round between player and computer that tells you if you won, lost or got a tie

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ function getComputerChoice(){
     :   random === 1 ? "paper"
     : "scissors";
 }
+function gameRound(playerChoice, computerChoice = getComputerChoice()){
+    return (playerChoice === "rock" && computerChoice === "scissors") 
+    || (playerChoice === "paper" && computerChoice === "rock")
+    || (playerChoice === "scissors" && computerChoice === "paper") ? alert(`You WIN!\nYour ${playerChoice} beats the opponent's ${computerChoice}.`)
+    : (computerChoice === "rock" && playerChoice === "scissors") 
+    || (computerChoice === "paper" && playerChoice === "rock")
+    || (computerChoice === "scissors" && playerChoice === "paper") ? alert(`You LOSE!\nThe opponent's ${computerChoice} beats your ${playerChoice}.`)
+    : "It's a TIE!";
+}
