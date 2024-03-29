e771ff2b6c6d21f699c4e70238af00223866a777
Waleed-IEE <iee.twaleed@gmail.com>
2024-03-29 17:07:00 +0100

    feat: Add main rock, paper and scissors game starter function including total number of rounds with a score system

--- js/script.js
+++ js/script.js
@@ -16,7 +16,29 @@ function gameRound(caseSensitivePlayerChoice, computerChoice = getComputerChoice
             :  "It's a TIE!";
     }
     else{
-        alert("Invalid input! Please type either rock, paper or scissors to play the game...");
+        alert("Invalid input! Please type either rock, paper or scissors to correctly play the game...");
     }
 
+}
+function playGame(rounds = 5){
+    let playerScore = 0, computerScore = 0;
+    for(i = 0;i < rounds;i++){
+        let result = gameRound(prompt("Play this game by typing either rock, paper or scissors!", getComputerChoice()));
+        if (result.match("WIN")){
+            ++playerScore;
+        }
+        else if (result.match("LOSE")){
+            ++computerScore;
+        }
+        console.log(result);
+    }
+    if (playerScore > computerScore){
+        alert(`The final score out of ${rounds} round(s) is Player ${playerScore}-${computerScore} Computer\nCongratulations for beating the Computer!`);
+    }
+    else if (computerScore > playerScore){
+        alert(`The final score out of ${rounds} round(s) is Player ${playerScore}-${computerScore} Computer\nGood luck next time!`);
+    }
+    else{
+        alert(`The final score out of ${rounds} round(s) is Player ${playerScore}-${computerScore} Computer\nYou both WIN!`);
+    }
 }
