7f69863ae57cb8500851f3ffb743db3bf606b97e
Waleed-IEE <iee.twaleed@gmail.com>
2024-03-29 16:22:17 +0100

    fix: Update game popup messages to return a string instead of alert

--- js/script.js
+++ js/script.js
@@ -9,11 +9,11 @@ function gameRound(caseSensitivePlayerChoice, computerChoice = getComputerChoice
         playerChoice = caseSensitivePlayerChoice.toLowerCase();
         return (playerChoice === "rock" && computerChoice === "scissors") 
             || (playerChoice === "paper" && computerChoice === "rock")
-            || (playerChoice === "scissors" && computerChoice === "paper") ? alert(`You WIN!\nYour ${playerChoice} beats the opponent's ${computerChoice}.`)
+            || (playerChoice === "scissors" && computerChoice === "paper") ? `You WIN!\nYour ${playerChoice} beats the opponent's ${computerChoice}.`
             :  (computerChoice === "rock" && playerChoice === "scissors") 
             || (computerChoice === "paper" && playerChoice === "rock")
-            || (computerChoice === "scissors" && playerChoice === "paper") ? alert(`You LOSE!\nThe opponent's ${computerChoice} beats your ${playerChoice}.`)
-            :  alert("It's a TIE!");
+            || (computerChoice === "scissors" && playerChoice === "paper") ? `You LOSE!\nThe opponent's ${computerChoice} beats your ${playerChoice}.`
+            :  "It's a TIE!";
     }
     else{
         alert("Invalid input! Please type either rock, paper or scissors to play the game...");
