c88b63a2420e11f0476b0ed23bb9a5f1a3e393a7
Waleed-IEE <iee.twaleed@gmail.com>
2024-04-04 18:51:24 +0100

    feat: Add three buttons for rock, paper and scissors then add an event listener for each one

--- js/script.js
+++ js/script.js
@@ -4,41 +4,19 @@ function getComputerChoice(){
     :   random === 1 ? "paper"
     : "scissors";
 }
-function gameRound(caseSensitivePlayerChoice, computerChoice = getComputerChoice()){
-    if (typeof caseSensitivePlayerChoice === "string"){
-        playerChoice = caseSensitivePlayerChoice.toLowerCase();
-        return (playerChoice === "rock" && computerChoice === "scissors") 
-            || (playerChoice === "paper" && computerChoice === "rock")
-            || (playerChoice === "scissors" && computerChoice === "paper") ? `You WIN!\nYour ${playerChoice} beats the opponent's ${computerChoice}.`
-            :  (computerChoice === "rock" && playerChoice === "scissors") 
-            || (computerChoice === "paper" && playerChoice === "rock")
-            || (computerChoice === "scissors" && playerChoice === "paper") ? `You LOSE!\nThe opponent's ${computerChoice} beats your ${playerChoice}.`
-            :  "It's a TIE!";
-    }
-    else{
-        alert("Invalid input! Please type either rock, paper or scissors to correctly play the game...");
+function gameRound(playerChoice, computerChoice = getComputerChoice()){
+    
+    return (playerChoice === "rock" && computerChoice === "scissors") 
+        || (playerChoice === "paper" && computerChoice === "rock")
+        || (playerChoice === "scissors" && computerChoice === "paper") ? `You WIN!\nYour ${playerChoice} beats the opponent's ${computerChoice}.`
+        :  (computerChoice === "rock" && playerChoice === "scissors") 
+        || (computerChoice === "paper" && playerChoice === "rock")
+        || (computerChoice === "scissors" && playerChoice === "paper") ? `You LOSE!\nThe opponent's ${computerChoice} beats your ${playerChoice}.`
+        :  "It's a TIE!";
     }
 
-}
-function playGame(rounds = 5){
-    let playerScore = 0, computerScore = 0;
-    for(i = 0;i < rounds;i++){
-        let result = gameRound(prompt("Play this game by typing either rock, paper or scissors!", getComputerChoice()));
-        if (result.match("WIN")){
-            ++playerScore;
-        }
-        else if (result.match("LOSE")){
-            ++computerScore;
-        }
-        console.log(result);
-    }
-    if (playerScore > computerScore){
-        alert(`The final score out of ${rounds} round(s) is Player ${playerScore}-${computerScore} Computer\nCongratulations for beating the Computer!`);
-    }
-    else if (computerScore > playerScore){
-        alert(`The final score out of ${rounds} round(s) is Player ${playerScore}-${computerScore} Computer\nGood luck next time!`);
-    }
-    else{
-        alert(`The final score out of ${rounds} round(s) is Player ${playerScore}-${computerScore} Computer\nYou both WIN!`);
-    }
-}
+const rockButton = document.createElement("button"), paperButton = document.createElement("button"), scissorsButton = document.createElement("button");
+
+rockButton.addEventListener("click", () => gameRound("rock"));
+paperButton.addEventListener("click", () => gameRound("paper"));
+scissorsButton.addEventListener("click", () => gameRound("scissors"));
